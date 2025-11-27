4710ae28cd33e78beb9454d5cd554ea0d9fda093
Paweł Rudnicki <pawelrudnicki79@gmail.com>
2025-11-27 21:31:28 +0100

    fix prisma and pages in aplications

---
+++ app/(application)/(app)/subscriptions/page.tsx
@@ -0,0 +1,7 @@
+export default function SubscriptionPage() {
+    return (
+        <>
+            <h1>Subscription page</h1>
+        </>
+    )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="text-3xl">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
