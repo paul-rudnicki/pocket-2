c8c58f42c8467e1f8c9026e122f961609590194f
Paweł Rudnicki <pawelrudnicki79@gmail.com>
2025-11-27 22:09:30 +0100

    add cleark

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -12,8 +20,25 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body>
+                    <header className="flex justify-end items-center p-4 gap-4 h-16">
+                        <SignedOut>
+                            <SignInButton />
+                            <SignUpButton>
+                                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                                </button>
+                            </SignUpButton>
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </header>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     )
 }
